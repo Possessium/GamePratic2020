72bd2f4221bae54732e6fa427430c066c95c12e6
G <hugo.guerquin@outlook.fr>
2020-10-10 20:49:36 +0200

    boucle done

--- Assets/Hugo/Scripts/GP_Camera.cs
+++ Assets/Hugo/Scripts/GP_Camera.cs
@@ -5,6 +5,8 @@ using Substance.Game;
 
 public class GP_Camera : MonoBehaviour
 {
+    public static GP_Camera I { get; private set; }
+
     [SerializeField] CameraState currentCamstate = CameraState.Free;
     [SerializeField] LayerMask interactableLayer = 0;
 
@@ -13,20 +15,34 @@ public class GP_Camera : MonoBehaviour
     [SerializeField] GameObject map = null;
 
 
+    private void Awake()
+    {
+        I = this;
+    }
 
     private void Update()
     {
+        if (!bell && GP_GameManager.I.SelectedBell)
+        {
+            bell = GP_GameManager.I.SelectedBell.GetComponent<GP_BellGuider>().Bell;
+        }
         if (!GP_GameManager.I.IsPlay) return;
         if (currentCamstate == CameraState.Free) Free();
     }
 
     public void ChangeToFree() => ChangeState(CameraState.Free);
 
-    void ChangeState(CameraState _state)
+    public void ChangeState(CameraState _state)
     {
+        if (!bell || !note || !map) return;
         note.SetActive(false);
         bell.SetActive(false);
         map.SetActive(false);
+        if (_state == CameraState.Bell)
+        {
+            transform.eulerAngles = new Vector3(30, 0, 0);
+            bell.SetActive(true);
+        }
         if (_state == CameraState.Free) Cursor.lockState = CursorLockMode.Locked;
         else Cursor.lockState = CursorLockMode.None;
         currentCamstate = _state;
@@ -34,6 +50,7 @@ public class GP_Camera : MonoBehaviour
 
     void Free()
     {
+        if (!bell || !note || !map) return;
         float _y = transform.eulerAngles.y + Input.GetAxis("Mouse X");
         float _x = transform.eulerAngles.x - Input.GetAxis("Mouse Y");
 
